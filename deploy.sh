e3712906a4d304abecfd54a5e5edbdb60e25069d
Ertsul <179445828@qq.com>
2021-12-04 23:36:33 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-#pm run docs:build
+#npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,11 +12,11 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git add -A
-# git commit -m 'deploy'
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:Ertsul/Ertsul.github.io.git master
+git push -f git@github.com:Ertsul/Ertsul.github.io.git
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
